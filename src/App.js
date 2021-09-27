3251da6bc7b30e7cf11a6f7087804a94e82ac052
jeanettephung <jeanettephung@hotmail.com>
2021-09-27 16:45:16 -0700

    Update ZingGrid, add registerMethod() example, and fix routing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Ajax from './components/Ajax';
 import Methods from './components/Methods';
 import Events from './components/Events';
 import ConditionalRendering from './components/ConditionalRendering';
+import RegisterMethod from './components/RegisterMethod';
 import { Switch, Route, Link } from 'react-router-dom';
 
 class App extends Component {
@@ -14,20 +15,22 @@ class App extends Component {
     return (
       <div className="App">
         <nav>
-            <Link to="/readonly">Read-Only</Link>
-            <Link to="/one-way-binding">One-Way Binding</Link>
-            <Link to="/two-way-binding">Two-Way Binding</Link>
-            <Link to="/conditional-rendering">Conditional Rendering</Link>
-            <Link to="/ajax">Ajax</Link>
-            <Link to="/methods">Methods</Link>
-            <Link to="/events">Events</Link>
+          <Link to="/read-only">Read-Only</Link>
+          <Link to="/one-way-binding">One-Way Binding</Link>
+          <Link to="/two-way-binding">Two-Way Binding</Link>
+          <Link to="/conditional-rendering">Conditional Rendering</Link>
+          <Link to="/register-method">Register Method</Link>
+          <Link to="/ajax">Ajax</Link>
+          <Link to="/methods">Methods</Link>
+          <Link to="/events">Events</Link>
         </nav>
         
-        <Route path='/' component={ReadOnly}/>
-        <Route path='/readonly' component={ReadOnly}/>
+        <Route path='/' exact component={ReadOnly}/>
+        <Route path='/read-only' component={ReadOnly}/>
         <Route path='/one-way-binding' component={OneWayBinding}/>
         <Route path='/two-way-binding' component={TwoWayBinding}/>
         <Route path='/conditional-rendering' component={ConditionalRendering}/>
+        <Route path='/register-method' component={RegisterMethod}/>
         <Route path='/ajax' component={Ajax}/>
         <Route path='/methods' component={Methods}/>
         <Route path='/events' component={Events}/>
